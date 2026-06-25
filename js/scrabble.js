5ec79871ae3f7e30faa37e4cd57d9a137fa58cc3
Chicxilulb <chixilulb.2004@gmail.com>
2026-06-25 18:19:46 -0400

    added fullboard functionality

--- js/scrabble.js
+++ js/scrabble.js
@@ -1,10 +1,60 @@
 
-
 $( document ).ready(function() {
     ScrabbleTiles.length = 28;
     
     generateTiles(ScrabbleTiles, 7);
-    $('.tile').draggable();
+    $('.tile').draggable({
+        revert : true
+    });
+
+    // add a table to the scrabble board to mark tile locations
+    if($('h1').html() == ' One-line scrabble '){
+
+        $('#one-line-img').droppable({
+            drop : function(event, ui) {
+                var object = ui.draggable;
+                $(this).parent().append(object);
+            }
+        });
+
+        var position;
+        $('#one-line-div').append('<table id="tile-positions"></table>');
+        $('#tile-positions').append('<thead><tr></tr></thead>');
+        
+        for (let y = 0; y < 15; y++){
+            var object = $.parseHTML('<td></td>');
+            $('tr').append(object);
+            position = $(object).position();
+            $(object).html(position.left+','+position.top);
+        }
+        $('#one-line-img').append();
+
+    } else {
+
+        $('#full-board-div').append('<table id="tile-positions"></table>');
+
+        console.log($('tr').html());
+        for (let x = 0; x < 15; x++) {
+
+        var tr = $.parseHTML('<tr></tr>');
+            for (let y = 0; y < 15; y++) {
+                var td = $.parseHTML('<td></td>');
+                $(tr).append(td);
+            }
+        $('#tile-positions').append(tr);
+        }
+
+        $('td').droppable({
+            drop : function(event, ui) {
+                var object = ui.draggable;
+                $(this).append(object);
+            }
+        });
+
+
+    }
+    
+    
 });
 
 function generateTiles(array, num = 7) {
@@ -14,15 +64,14 @@ function generateTiles(array, num = 7) {
         var tile = keys[random];
 
         if(tile == '_') {
-             $( "#div-holder" ).append( "<div class='tile'><img src='images/Scrabble_Tiles/Scrabble_Tile_Blank.jpg'  alt='Failed to load tile'></div>" );
+             $( "#holder-div" ).append( "<div class='tile'><img src='images/Scrabble_Tiles/Scrabble_Tile_Blank.jpg'  alt='Failed to load tile'></div>" );
         } else {
-            $( "#div-holder" ).append( "<div class='tile'><img src='images/Scrabble_Tiles/Scrabble_Tile_"+tile+".jpg'  alt='Failed to load tile'></div>" );
+            $( "#holder-div" ).append( "<div class='tile'><img src='images/Scrabble_Tiles/Scrabble_Tile_"+tile+".jpg'  alt='Failed to load tile'></div>" );
         }
 
         if(array[tile]["number-remaining"]-- <= 0){
             array.length--;
             delete(array[tile]);
-            console.log(array);
         }
         
     }

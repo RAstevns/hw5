a69e3e229b164cf8d7b5e1fd41819b9d26d8ca10
Chicxilulb <chixilulb.2004@gmail.com>
2026-06-26 17:01:17 -0400

    special squares

--- js/scrabble.js
+++ js/scrabble.js
@@ -1,72 +1,142 @@
 
+// row : { column : value}
+// values 
+// center -1
+// triple word 4
+// triple letter 3
+// double word 2
+// double letter 1
+// normal 0 
+var wordDirection = '';
+
+var valuetable = [
+    [4,0,0,1,0,0,0,4,0,0,0,1,0,0,4],
+    [0,2,0,0,0,3,0,0,0,3,0,0,0,2,0],
+    [0,0,2,0,0,0,1,0,1,0,0,0,2,0,0],
+    [1,0,0,2,0,0,0,1,0,0,0,2,0,0,1],
+    [0,0,0,0,2,0,0,0,0,0,2,0,0,0,0],
+    [0,3,0,0,0,3,0,0,0,3,0,0,0,3,0],
+    [0,0,1,0,0,0,1,0,1,0,0,0,1,0,0],
+    [4,0,0,1,0,0,0,-1,0,0,0,1,0,0,4],
+    [0,0,1,0,0,0,1,0,1,0,0,0,1,0,0],
+    [0,3,0,0,0,3,0,0,0,3,0,0,0,3,0],
+    [0,0,0,0,2,0,0,0,0,0,2,0,0,0,0],
+    [1,0,0,2,0,0,0,1,0,0,0,2,0,0,1],
+    [0,0,2,0,0,0,1,0,1,0,0,0,2,0,0],
+    [0,2,0,0,0,3,0,0,0,3,0,0,0,2,0],
+    [4,0,0,1,0,0,0,4,0,0,0,1,0,0,4]
+];
+// Init
 $( document ).ready(function() {
     ScrabbleTiles.length = 28;
     
     generateTiles(ScrabbleTiles, 7);
+
     $('.tile').draggable({
         revert : true
     });
 
+    $('#submit-word').on( "click", function(){
+        
+    });
+
+
+
     // add a table to the scrabble board to mark tile locations
     if($('h1').html() == ' One-line scrabble '){
-
-        $('#one-line-img').droppable({
-            drop : function(event, ui) {
-                var object = ui.draggable;
-                $(this).parent().append(object);
-            }
+        $('#one-line-div').append('<table id="tile-positions"></table>');
+        // setting offset for table
+        $("#tile-positions").css({
+            top: '-85px',
+            left: '5px'
         });
 
-        var position;
-        $('#one-line-div').append('<table id="tile-positions"></table>');
-        $('#tile-positions').append('<thead><tr></tr></thead>');
-        
-        for (let y = 0; y < 15; y++){
-            var object = $.parseHTML('<td></td>');
-            $('tr').append(object);
-            position = $(object).position();
-            $(object).html(position.left+','+position.top);
-        }
-        $('#one-line-img').append();
+        tableGen(1, 15);
 
     } else {
 
         $('#full-board-div').append('<table id="tile-positions"></table>');
+        $("#tile-positions").css({
+            top: '-1340px',
+            left: '200px'
+        });
+        tableGen(15, 15);
+        $('#holder-div').css({
+            top: '1600px',
+            left: '400px'
+        });
+    }
 
-        console.log($('tr').html());
-        for (let x = 0; x < 15; x++) {
+    $('td').droppable({
+        drop : function(event, ui) {
+            var object = ui.draggable;
 
+            if(wordDirection == 'y'){
+                $("[y|='"+(y - 1)+"']:empty").droppable('enable');
+                $("[y|='"+(y + 1)+"']:empty").droppable('enable');
+            }
+            if(wordDirection == 'x'){
+                $("[x|='"+(x - 1)+"']:empty").droppable('enable');
+                $("[x|='"+(x + 1)+"']:empty").droppable('enable');
+            }
+                
+            $('td').droppable('disable');   
+            object.draggable('disable');
+
+            var x = parseInt($(this).attr('x'));
+            var y = parseInt($(this).attr('y'));
+            
+            console.log(y+': '+x);
+            
+            
+
+            $(this).droppable('disable');
+            $(this).append(object);
+        }
+    });
+    
+});
+
+    // helper function for readability
+function tableGen(width, height) {
+    for (let x = 0; x < width; x++) {
         var tr = $.parseHTML('<tr></tr>');
-            for (let y = 0; y < 15; y++) {
+            for (let y = 0; y < height; y++) {
                 var td = $.parseHTML('<td></td>');
+                switch(valuetable[y][x]){
+                    case 1:
+                        $(td).addClass('double-letter');
+                        break;
+                    case 2:
+                        $(td).addClass('double-word');
+                        break;
+                    case 3: 
+                        $(td).addClass('triple-letter');
+                        break;
+                    case 4:
+                        $(td).addClass('triple-word');
+                        break;
+                    case -1:
+                        $(td).attr("id","center");
+                    default:
+                }
+                $(td).attr("x",x);
+                $(td).attr("y",y);
                 $(tr).append(td);
             }
         $('#tile-positions').append(tr);
-        }
-
-        $('td').droppable({
-            drop : function(event, ui) {
-                var object = ui.draggable;
-                $(this).append(object);
-            }
-        });
-
-
     }
-    
-    
-});
+}
 
 function generateTiles(array, num = 7) {
     for(var i = 0; i < Number(num); i++){
         var keys = Object.keys(array);
         var random = Math.floor(Math.random() * (array.length-1));
         var tile = keys[random];
-
         if(tile == '_') {
-             $( "#holder-div" ).append( "<div class='tile'><img src='images/Scrabble_Tiles/Scrabble_Tile_Blank.jpg'  alt='Failed to load tile'></div>" );
+             $( "#holder-img" ).after( "<div class='tile'><img  src='images/Scrabble_Tiles/Scrabble_Tile_Blank.jpg'  alt='Failed to load tile'></div>" );
         } else {
-            $( "#holder-div" ).append( "<div class='tile'><img src='images/Scrabble_Tiles/Scrabble_Tile_"+tile+".jpg'  alt='Failed to load tile'></div>" );
+            $( "#holder-img" ).after( "<div class='tile'><img  src='images/Scrabble_Tiles/Scrabble_Tile_"+tile+".jpg'  alt='Failed to load tile'></div>" );
         }
 
         if(array[tile]["number-remaining"]-- <= 0){

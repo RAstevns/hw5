ad77b9f248c87c04f631f9a250fed52ff0b5f51f
Chicxilulb <chixilulb.2004@gmail.com>
2026-06-28 17:58:12 -0400

    finished extra credit

--- js/scrabble.js
+++ js/scrabble.js
@@ -33,16 +33,30 @@ var valuetable = [
     [0, 2, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 2, 0],
     [4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4]
 ];
-// Init
+
+// get the words file from system
+var words;
+var totalscore = 0;
+var currentpointvalue = 0;
+var multiplier = 1;
+var state = 'unset';
+
+$.get('/usr/share/dict/words', function (string) {
+    words = string;
+}, 'text');
+// Init 
 $(document).ready(function () {
     ScrabbleTiles.length = 28;
 
     generateTiles(ScrabbleTiles, 7);
+
     
-    var totalscore = 0;
     $('#restart').on("click", function () {
+        $('.error').remove();
         $('td').empty();
         $('td').droppable('enable');
+
+        state = 'unset';
 
         $('#current-word').empty();
         $('#holder-div div').remove();
@@ -56,32 +70,58 @@ $(document).ready(function () {
 
         currentpointvalue = 0;
         multiplier = 1;
-
+        totalscore = 0;
+        
         generateTiles(ScrabbleTiles);
     });
     $('#submit-word').on("click", function () {
 
-        var word = $('#current-word').html();
-        var value = (currentpointvalue * multiplier);
+        $('.error').remove();
 
-        $('td').empty();
+        let word = $('#current-word').html();
+        let value = (currentpointvalue * multiplier);
+
+        // make a regular expression to check if the word is in 'words' (converts blank to wildcard)
+        let regex = new RegExp(String.raw`\n`+word.replace('_', String.raw`\w`)+String.raw`\n`, 'ig');
+        
+        //regardless of the words validity the board still needs to be cleared
+
         $('td').droppable('enable');
-        $('td').removeAttr('state');
+        state = 'unset';
         $('td').removeAttr('letter');
 
         $('#current-word').empty();
         $('#point-value').empty();
 
-        // 7 (the number of tiles per hand)
-        // number of children in the div - the image =  number of cards in hand
-        generateTiles(ScrabbleTiles, 7 - ($("#holder-div").children().length - 1));
+        
 
-        $('#word-history').append('<li> ' + word + ': ' + value + ' </li>')
-        totalscore += (currentpointvalue * multiplier);
+        if (regex.test(words) && word.length > 1) {
+
+            // word is valid
+            $('td').empty();
+            // 7 (the number of tiles per hand)
+            // number of children in the div - the image =  number of cards in hand
+            generateTiles(ScrabbleTiles, 7 - ($("#holder-div").children().length - 1));
+
+            $('#word-history').append('<li> ' + word + ': ' + value + ' </li>')
+            
+            totalscore += (currentpointvalue * multiplier);
+            $('#score-holder').html(totalscore);
+
+        } else {
+
+            // word is invalid
+
+            $('.tile').each(function (index, element) {
+                $('#holder-div').append(element);
+                $(element).draggable('enable');
+            });
+
+            $('#restart').before('<p class="error" style="color:red;">Invalid word</p>');
+        }
+
         currentpointvalue = 0;
         multiplier = 1;
-        $('#score-holder').html(totalscore);
-
     });
 
     // add a table to the scrabble board to mark tile locations
@@ -109,81 +149,78 @@ $(document).ready(function () {
         });
     }
 
-    var currentpointvalue = 0;
-    var multiplier = 1;
+    
     $('td').droppable({
         drop: function (event, ui) {
-            var object = ui.draggable;
+            let object = ui.draggable;
 
-            var x = $(this).attr('x');
-            var y = $(this).attr('y');
+            let x = $(this).attr('x');
+            let y = $(this).attr('y');
 
-            $(object).addClass('unsubmitted');
             var letter = object.attr('letter');
+            // define scope for up/down/left/r
+            {
+                let up = $("[x|='" + x + "'][y|='" + (y - 1) + "']");
+                let down = $("[x|='" + x + "'][y|='" + (parseInt(y) + 1) + "']");
+                let left = $("[y|='" + y + "'][x|='" + (x - 1) + "']");
+                let right = $("[y|='" + y + "'][x|='" + (parseInt(x) + 1) + "']");
 
-            
-            var up = $("[x|='" + x + "'][y|='" + (y - 1) + "']");
-            var down = $("[x|='" + x + "'][y|='" + (parseInt(y) + 1) + "']");
+                if (state == 'unset') {
 
-            var left = $("[y|='" + y + "'][x|='" + (x - 1) + "']");
-            var right = $("[y|='" + y + "'][x|='" + (parseInt(x) + 1) + "']");
-            
-            
-            if (left.attr('state') == 'unknown' || $(this).attr('state') == 'horizontal') {
-                if (left.has('.unsubmitted').length) {
-                    left.attr('state', 'horizontal');
-                    $(this).attr('state', 'horizontal');
-                    right.attr('state', 'horizontal');
-                    right.droppable('enable');
-                    left.droppable('disable');
                     $('#current-word').append(letter);
-                }
-                if (right.has('.unsubmitted').length) {
-                    right.attr('state', 'horizontal');
-                    $(this).attr('state', 'horizontal');
-                    left.attr('state', 'horizontal');
-                    right.droppable('disable');
+                    state = 'unknown';
+                    $('td').droppable('disable');
+
+                    up.droppable('enable');
+                    down.droppable('enable');
                     left.droppable('enable');
+                    right.droppable('enable');
+
+                } else if (state == 'unknown') {
+
+                    $('td').droppable('disable');
+                    
+                    // if its not vertical then its horizontal
+                    state = (up.children().length || down.children().length ? 'vertical' : 'horizontal');
+
+                    // enable the one next to the initial tile
+                    if (up.children().length) {
+                        $("[x|='" + x + "'][y|='" + (y - 2) + "']").droppable('enable');
+                    } else{
+                        $("[x|='" + x + "'][y|='" + (parseInt(y) + 2) + "']").droppable('enable');
+                    }
+                    if (left.children().length) {
+                        $("[y|='" + y + "'][x|='" + (x - 2) + "']").droppable('enable');
+                    } else {
+                        $("[y|='" + y + "'][x|='" + (parseInt(x) + 2) + "']").droppable('enable');
+                    }
+
+                    
+
+                }
+            }
+            if (state == 'vertical') {
+                let up = $("[x|='" + x + "'][y|='" + (y - 1) + "']:empty");
+                let down = $("[x|='" + x + "'][y|='" + (parseInt(y) + 1) + "']:empty");
+                if (up.length) {
                     $('#current-word').prepend(letter);
+                    up.droppable('enable');
+                } else {
+                    $('#current-word').append(letter);
+                    down.droppable('enable');
                 }
             }
-            if ($(this).attr('state') == 'unknown' || $(this).attr('state') == 'vertical') {
-                if ($('h1').html() == ' Scrabble ') {
-                    if (down.has('.unsubmitted')) {
-                        down.attr('state', 'vertical');
-                        $(this).attr('state', 'vertical');
-                        up.attr('state', 'vertical');
-
-                        up.droppable('enable');
-                        $('#current-word').append(letter);
-                    }
-                    if (up.has('.unsubmitted')) {
-                        down.attr('state', 'vertical');
-                        $(this).attr('state', 'vertical');
-                        up.attr('state', 'vertical');
-
-                        down.droppable('enable');
-                        $('#current-word').prepend(letter);
-                    }
+            if (state == 'horizontal') {
+                let left = $("[y|='" + y + "'][x|='" + (x - 1) + "']:empty");
+                let right = $("[y|='" + y + "'][x|='" + (parseInt(x) + 1) + "']:empty");
+                if (left.length) {
+                    $('#current-word').prepend(letter);
+                    left.droppable('enable');
+                } else {
+                    $('#current-word').append(letter);
+                    right.droppable('enable');
                 }
             }
-            if (
-                !right.is('[state]') &&
-                !left.is('[state]') &&
-                !up.is('[state]') &&
-                !down.is('[state]')
-            ) {
-
-                $('#current-word').append(letter);
-                $(this).attr('state', 'unknown');
-                $('td').droppable('disable');
-
-                up.droppable('enable');
-                down.droppable('enable');
-                left.droppable('enable');
-                right.droppable('enable');
-            }
-            console.log($(this).attr('state') == 'unknown');
 
             object.draggable('disable');
 
@@ -264,11 +301,11 @@ function generateTiles(array, num = 7) {
             alert('game over');
             return;
         }
-
-        var random = Math.floor(Math.random() * (keys.length - 1));
+        var random = Math.floor(Math.random() * (keys.length));
         var tile = keys[random];
         if (tile == '_') {
-            $("#holder-img").after("<div class='tile' letter='Blank'><img  src='images/Scrabble_Tiles/Scrabble_Tile_Blank.jpg'  alt='Failed to load tile'></div>");
+
+            $("#holder-img").after("<div class='tile' letter='_'><img  src='images/Scrabble_Tiles/Scrabble_Tile_Blank.jpg'  alt='Failed to load tile'></div>");
         } else {
             $("#holder-img").after("<div class='tile' letter=" + tile + "><img  src='images/Scrabble_Tiles/Scrabble_Tile_" + tile + ".jpg'  alt='Failed to load tile'></div>");
         }
